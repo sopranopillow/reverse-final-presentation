263b21439514cec742e4861ceb82ecd580523977
Esteban Munoz <esteban.230@hotmail.com>
2020-10-14 18:13:58 -0600

    adding lodash

---
+++ src/App.styles.js
@@ -0,0 +1,11 @@
+export const App = {
+    display: 'block',
+    textAlign: 'center',
+    align: 'center'
+}
+
+export const headerUtepStyles = {
+    height: '104px',
+    width: '625px',
+    margin:'16px 0px'
+}
