da1fcbded6cb54a62aa166e8891231b269becbb5
Esteban Munoz <esteban.230@hotmail.com>
2020-10-15 22:31:32 -0600

    Finished website

--- src/pages/Grades.js
+++ src/pages/Grades.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as _ from 'lodash';
 import { Nav, Content } from '../Components';
 import './Grades.css';
 
@@ -11,17 +12,65 @@ class Home extends React.Component {
             loggedIn: false,
             username: '',
             password: '',
+            fileName: ''
         }
 
-        this.validateFiles = require(process.env.PUBLIC_URL + '/users/index.json');
+        this.validateFiles = require('../../public/users/index.json');
     }
 
     render() {
-        const table = <div>Hello</div>;
-        const loggedIn = false;
+        const Table = () => {
+            const { fileName , loggedIn } = this.state;
+            if (!loggedIn) return (<div>Log in please</div>)
+            const data = require('../../public/users/' + fileName + '.json');
+            const titleKeys = Object.keys(data)
+            const titles = titleKeys.map(key => <th className="th" >{key}</th>);
+            let index = 0;
+            const grades = titleKeys.map(titleKey => {
+                const assignmentKeys = Object.keys(data[titleKey]);
+                const t = assignmentKeys.map(assignmentKey => {
+                    return (
+                        <tr>
+                            <td className="td" >{assignmentKey}</td>
+                            {titleKeys.map(titleKey => {
+                                if (titleKey === titleKeys[index]){
+                                    return <td className="td" >{data[titleKey][assignmentKey]}</td>
+                                }
+                                return <td className="td" ></td>
+                            })}
+                        </tr>)
+                })
+                index++;
+                return t;
+            });
+
+            return (
+                <table className="table">
+                    <tbody>
+                        <tr><th className="th" >Assignemnt</th>{titles}</tr>
+                        {grades}
+                    </tbody>
+                </table>
+            )
+        }
 
         const validateUser = (event) => {
-            console.log(this.validateFiles)
+            const { username, password } = this.state;
+            const { userList } = this.validateFiles;
+            const userListKeys = Object.keys(userList);
+
+            _.merge({}, {'username': username, 'password': password});
+
+            if (userListKeys.includes(username) && userList[username].password === password){
+                this.setState({
+                    loggedIn: true,
+                    fileName: userList[username].file
+                }, () => {
+                    alert('Succesfully logged in');
+                })
+            }else {
+                alert('Wrong password or username')
+            }
             event.preventDefault();
         }
 
@@ -38,16 +87,16 @@ class Home extends React.Component {
         }
 
         const LogInForm = (
-            <form onSubmit={validateUser}>
-                <div>
+            <form className="form" onSubmit={validateUser}>
+                <div className="formItem">
                     <div>Username</div>
                     <input onChange={handleUsername}/>
                 </div>
-                <div>
+                <div className="formItem">
                     <div>Password</div>
-                    <input onChange={handlePassword}/>
+                    <input type="password" onChange={handlePassword}/>
                 </div>
-                <input type="submit" value="Log in"/>
+                <input className="formItem" type="submit" value="Log in"/>
             </form>
         )
 
@@ -55,8 +104,9 @@ class Home extends React.Component {
             <div className="Grades">
                 <Nav location="Grades"/>
                 <Content>
-                    {loggedIn && table}
-                    {!loggedIn && LogInForm}
+                    <div className="data">
+                        {this.state.loggedIn ? (<Table/>) : LogInForm}
+                    </div>
                 </Content>
             </div>
         );
